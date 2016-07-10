1f6301283d55bf415b0e9dcfc8467b70f3f5a785
Gaston Zanitti <gzanitti@gmail.com>
2016-07-10 15:30:15 -0300

    Meto un principio de Logger

--- tp1/src2/logger.py
+++ tp1/src2/logger.py
@@ -1,41 +1,41 @@
 class Logger(object):
 
     def loggearinicioPosesion(equipoAtacante):
-        //arranca atacando tal equipo
+        #arranca atacando tal equipo
         return true
 
     def loggearPelotaAfuera():
-        //la pelota se fue
+        #la pelota se fue
         return true
 
     def loggearPaseExitoso(paseEmisor, paseReceptor, paseInterceptor):
-        //pase exitoso entre A y B, que C no logra interceptar
+        #pase exitoso entre A y B, que C no logra interceptar
         return true
 
     def loggearBloqueo(tirador, bloqueador):
-        //A le bloquea el tiro a B
+        #A le bloquea el tiro a B
         return true
 
     def loggearIntercepcionExito(tirador, bloqueador):
-        //A le intercepta el pase a B
+        #A le intercepta el pase a B
         return true
 
     def loggearPaseIntercepcionFracaso(paseEmisor, paseReceptor, paseInterceptor):
-        //A no se la logra pasar a B y C no la logra interceptar, sale de la cancha.
+        #A no se la logra pasar a B y C no la logra interceptar, sale de la cancha.
         return true
 
     def loggearEncesatarDoble(tirador, bloqueador):
-        //A lanza un doble, B no lo logra bloquear, y convierte
+        #A lanza un doble, B no lo logra bloquear, y convierte
         return true
 
     def loggearEncesatarTriple(tirador, bloqueador):
-        //A lanza un triple, B no lo logra bloquear, y convierte
+        #A lanza un triple, B no lo logra bloquear, y convierte
         return true
 
     def loggearReboteoIntento(jugador):
-        //El jugador A deel equipo B, intenta agarrar un rebote
+        #El jugador A deel equipo B, intenta agarrar un rebote
         return true
 
     def loggearReboteoExito(jugador):
-        //El jugador A deel equipo B, logra agarrar un rebote
+        #El jugador A deel equipo B, logra agarrar un rebote
         return true
