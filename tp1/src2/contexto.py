92e70481770abbe5a8c7c7da6ba7c1e3b979cf04
Matias Laporte <matiaslaporte@gmail.com>
2016-07-10 17:18:53 -0300

    me habia olvidado del contexto

---
+++ tp1/src2/contexto.py
@@ -0,0 +1,36 @@
+class Contexto(object):
+    def equipoAtacante(self, unEquipoOfensivo=None):
+        if(unEquipoOfensivo is None):
+            return self._equipoAtacante
+        else:
+            self._equipoAtacante = unEquipoOfensivo
+
+    def equipoDefensor(self, unEquipoDefensor=None):
+        if(unEquipoDefensor is None):
+            return self._equipoDefensor
+        else:
+            self._equipoDefensor = unEquipoDefensor
+
+    def jugadorConPosesion(self, unJugadorOfensivo=None):
+        if(unJugadorOfensivo is None):
+            return self._jugadorConPosesion
+        else:
+            self._jugadorConPosesion = unJugadorOfensivo
+
+    def pasoDeJugadaOfensiva(self, pasoJugadaOfensiva=None):
+        if(pasoDeJugadaOfensiva is None):
+            return self._pasoDeJugadaOfensiva
+        else:
+            self._pasoDeJugadaOfensiva = pasoJugadaOfensiva
+
+    def jugadaOfensiva(self, unaJugadaOfensiva=None):
+        if(unaJugadaOfensiva is None):
+            return self._jugadaOfensiva
+        else:
+            self._jugadaOfensiva = unaJugadaOfensiva
+
+    def jugadaDefensiva(self, unaJugadaDefensiva=None):
+        if(unaJugadaDefensiva is None):
+            return self._jugadaDefensiva
+        else:
+            self._jugadaDefensiva = unaJugadaDefensiva
